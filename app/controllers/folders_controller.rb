1397006d8bb6b08a0949cd269f4c339621e4da65
Phil Bottomley <philbottomley1@gmail.com>
2011-12-07 12:33:21 +0000

    cleanup folders controller

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,23 +1,25 @@
 class FoldersController < ApplicationController
 
-  before_filter :find_folder, :only=> [:show, :details, :browse, :new, :download]
+  before_filter :find_folder, :only => [:show, :details, :browse, :new, :download]
   
-  after_filter :log_folder, :only=> [:create, :update]
+  after_filter :log_folder, :only => [:create, :update]
   
   skip_after_filter :log, :only => :folderChildren
   
   def index
     @canHome = current_user.can_home?
     @folders = []
+    
     Folder.where('folders.parent_id is null or folders.parent_id = 0').order(:name).each do |folder|
       if folder.canread(current_user) || folder.canWrite(current_user)
         @folders << folder
       end
     end
+
     if current_user.is_admin?
-      @assets  = Asset.where(:folder_id=>nil).order(:uploaded_file_file_name)
+      @assets  = Asset.where(:folder_id => nil).order(:uploaded_file_file_name)
     else
-      @assets  = Asset.where(:folder_id=>nil, :user_id=>current_user).order(:uploaded_file_file_name)  
+      @assets  = Asset.where(:folder_id => nil, :user_id => current_user).order(:uploaded_file_file_name)  
     end
     
     @log_action = "browse"
@@ -43,6 +45,7 @@ class FoldersController < ApplicationController
     @folder.user_id = current_user.id
     @folder.inherit_permissions = true unless current_user.is_admin
     @folder.inherit_permissions = false unless @folder.parent_id?
+    
     if @folder.save
       if @folder.parent_id?
         redirect_to browse_path(@folder.parent)  
@@ -63,6 +66,7 @@ class FoldersController < ApplicationController
 
   def update
     @folder = Folder.find(params[:id])
+    
     if @folder.update_attributes(params[:folder])
       if @folder.parent.nil?
         redirect_to root_path, :notice  => "Successfully updated folder."
@@ -76,6 +80,7 @@ class FoldersController < ApplicationController
 
   def browse
     @canHome = current_user.can_home
+    
     if @current_folder
       #folders
       @folders = current_user.accessible_folders.where(:parent_id => @current_folder).order("name")
@@ -100,6 +105,7 @@ class FoldersController < ApplicationController
     else
       @parentFolder = Folder.find(params[:folder_id])
     end
+
     @folders = current_user.accessible_folders.where(:parent_id => @parentFolder).order(:name)
   end
 
@@ -110,6 +116,7 @@ class FoldersController < ApplicationController
     @searchNotes = search_query[:notes] == '1'
     
     folders = current_user.accessible_folders
+    
     if @searchNotes
       @folders = folders.find(:all, :conditions => ["name ILIKE ? OR notes ILIKE ?", @escaped_query, @escaped_query]) if folders
       @assets = current_user.assets.find(:all, :conditions => ["uploaded_file_file_name ILIKE ? OR notes ILIKE ?", @escaped_query, @escaped_query])
@@ -131,6 +138,7 @@ class FoldersController < ApplicationController
     @folder = Folder.find(params[:id])
     @log_target_id = @folder.id
     @log_file_path = "/" + @folder.breadcrumbs
+    
     if @folder.parent.nil?
       @redirect = root_path
     else
@@ -159,20 +167,25 @@ class FoldersController < ApplicationController
   
   def move
     @folders = []
+    
     Folder.find(params[:ids].split(',')).each do |folder|
       (folder.canread?(@current_user) && folder.canwrite?(@current_user)) ? @folders << folder : flash[:error] = "Unauthorised" and redirect_to root_path and return
     end
+
     @log_file_path = ""
+
     if @folders.first.parent.nil?
       @log_file_path = "/"
     else
       @log_file_path = @folders.first.breadcrumbs + "/"
     end
+    
     @log_target_id = @folders.collect{|a| a.id}.join(', ')
     
     if @folders.count > 1
       @log_file_path += ": "
-    end    
+    end
+
     @log_file_path += @folders.collect{|a| a.name}.join(', ')
   end 
   
@@ -181,16 +194,20 @@ class FoldersController < ApplicationController
     require 'zip/zipfilesystem'
     @log_file_path = ""
     t = Tempfile.new("downloadZip#{request.remote_ip}")
+
     Zip::ZipOutputStream.open(t.path) do |zos|
       unless params[:folders].blank?
         @downloadFolders = Folder.find(params[:folders].split(','))
         #Download each folder
+        
         if @downloadFolders.first.parent_id.nil?
           @log_file_path << "/"
         else
           @log_file_path << @downloadFolders.first.parent.name
         end
+
         @log_file_path << ": "
+        
         @downloadFolders.each do |parentFolder|
           @folders = parentFolder.descendant_folders_include_self_can_read(current_user)
           @folders.each do |folder|
@@ -200,6 +217,7 @@ class FoldersController < ApplicationController
             end
           end
         end
+        
         @log_file_path += @downloadFolders.collect{|f| f.name+"/"}.join(', ')
       end
       
