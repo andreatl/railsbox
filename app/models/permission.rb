ce35693dbcd1d536a4a0b9c6fa8eaf9cf232d3ed
Phil Bottomley <philbottomley1@gmail.com>
2011-12-07 12:17:52 +0000

    cleanup permission model

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -4,6 +4,7 @@ class Permission < ActiveRecord::Base
   belongs_to :folder
   
   validates_presence_of :assigned_by
-  validates_inclusion_of :parent_type, :in=>['User','Group']
-  validates_uniqueness_of :folder_id, :scope=>[:parent_id, :parent_type]
+  validates_inclusion_of :parent_type, :in => ['User','Group']
+  validates_uniqueness_of :folder_id, :scope => [:parent_id, :parent_type]
+
 end
