526ddbdd04d5c7e8bc95cbe4f313df2d75849ed0
Yule <paul@dryule.com>
2011-12-09 16:49:59 +0000

    failing spec for circular reference

--- spec/models/folder_spec.rb
+++ spec/models/folder_spec.rb
@@ -8,7 +8,14 @@ describe Folder do
   it "should be valid" do
   	valid_folder.should be_valid
   end
-  
+
+  it "should not allow itself to be a parent" do
+    fl = valid_folder
+    fl.save
+    fl.parent_id = fl.id
+    fl.should_not be_valid
+  end
+
 end
 
 
