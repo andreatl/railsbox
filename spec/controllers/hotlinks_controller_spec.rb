6fe6b92ddde084ad632a9ba8ed079d928a8abf1a
Yule <paul@dryule.com>
2011-12-14 17:27:37 +0000

    fixing controller test

--- spec/controllers/hotlinks_controller_spec.rb
+++ spec/controllers/hotlinks_controller_spec.rb
@@ -16,7 +16,7 @@ describe HotlinksController do
 
 
   it "show action should render show template" do
-    get :show, :id => Hotlink.first.id
+    get :show, :link => Hotlink.first.link
     response.should render_template(:show)
   end
 
