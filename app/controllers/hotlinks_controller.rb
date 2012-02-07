1d35d4927caf122ea6620e8762abbea69383873e
Phil Bottomley <philbottomley1@gmail.com>
2012-02-07 11:31:33 +0000

    fix hotlinking

--- app/controllers/hotlinks_controller.rb
+++ app/controllers/hotlinks_controller.rb
@@ -1,19 +1,19 @@
 class HotlinksController < ApplicationController
-  
+
   before_filter :check_hotlink, :except=>[:show, :update]
-  
+
   before_filter :check_admin, :only=> [:index]
 
   skip_before_filter :is_authorised, :only=>[:show,:update] #can view hotlink without login
-  
+
   after_filter :logFilePath, :except=>[:new, :index]
-  
+
   def index
     @hotlinks = Hotlink.all
   end
 
   def show
-    @hotlink = Hotlink.first(:conditions=> {:link => params[:link]})
+    @hotlink = Hotlink.find_by_link(params[:id])
     respond_to do |format|
       format.html
       format.js { render :layout => false }
@@ -39,16 +39,16 @@ class HotlinksController < ApplicationController
       render :action => 'new'
     end
   end
-  
+
   def link
     @hotlink = Hotlink.find(params[:hotlink_id])
   end
-  
+
   def update
     #Download
     @hotlink = Hotlink.authenticate(params[:id], params[:hotlink][:password])
     @log_action = "Access"
-    
+
     if @hotlink
       send_file @hotlink.asset.uploaded_file.path, :type => @hotlink.asset.uploaded_file_content_type, :filename => @hotlink.asset.uploaded_file_file_name
     else
@@ -58,23 +58,23 @@ class HotlinksController < ApplicationController
       render "show"
     end
   end
-  
+
   def destroy
     @hotlink = Hotlink.find(params[:id])
     @hotlink.destroy
     redirect_to hotlinks_path, :notice => "Successfully deleted."
   end
-  
+
   private
   def logFilePath
     @log_file_path = ""
-    
-    if @hotlink.asset 
+
+    if @hotlink.asset
       @log_file_path = @hotlink.asset.folder.breadcrumbs if !@hotlink.asset.folder.nil?
       @log_file_path << "/" + @hotlink.asset.uploaded_file_file_name
     else
     	@log_file_path << "Not known"
-    end	
+    end
 
     @log_target_id = @hotlink.id
   end
